3f02cf5defe2d0bbbd2b63753428b8488f29bd1c
raccoonrocket <9802432172sergei@gmail.com>
2023-11-21 23:19:48 +0300

    Добавьте файлы проекта.

---
+++ Computer graphics/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Computer_graphics
+{
+    internal class Game : GameWindow
+    {
+        // A simple constructor to let us set properties like window size, title, FPS, etc. on the window.
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        // This function runs on every update frame.
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            // Check if the Escape button is currently being pressed.
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                // If it is, close the window.
+                Close();
+            }
+
+            base.OnUpdateFrame(e);
+        }
+    }
+}
---
+++ Computer graphics/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Computer_graphics
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "Computer graphics",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            using (var game = new Game(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                game.Run();
+            }
+        }
+    }
+}
